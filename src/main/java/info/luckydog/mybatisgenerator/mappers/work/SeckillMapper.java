24eefa5ca79ff1fdf663453d5cc558ecedb6999e
eric <zanxu2016@gmail.com>
2020-03-03 17:19:03 +0800

    Initial commit

---
+++ src/main/java/info/luckydog/mybatisgenerator/mappers/work/SeckillMapper.java
@@ -0,0 +1,6 @@
+package info.luckydog.mybatisgenerator.mappers.work;
+
+import info.luckydog.mybatisgenerator.mappers.work.nochange.MBGSeckillMapper;
+
+public interface SeckillMapper extends MBGSeckillMapper {
+}
